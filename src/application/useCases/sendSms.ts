34b2327a59c465f63e91a18fcca0b484efc06bcc
Ulises Jaramillo <ulises.jaramillo@est.fi.uncoma.edu.ar>
2024-09-09 11:37:47 -0300

    funtions moved to a domain file

--- src/application/useCases/sendSms.ts
+++ src/application/useCases/sendSms.ts
@@ -26,7 +26,11 @@ export class SendSms {
       );
 
     // Format all the messages for sending
-    const formattedMessages = this.makeMessages(messages, "");
+    let formattedMessages = "";
+
+    messages.forEach((message) => {
+      formattedMessages += message.makeMessage();
+    });
 
     // Send the messages (Caution: This is currently set to always succeed)
     const success = await this.messageService.sendSmsBlock(formattedMessages);
@@ -40,101 +44,5 @@ export class SendSms {
     }
 
     return messages;
-  }
-
-  // Helper method to format messages for sending
-  private makeMessages(data: Message[], message: string) {
-    data.forEach((element) => {
-      message +=
-        element.paciente_celular +
-        "," +
-        element.paciente_celular +
-        "," +
-        this.addTextFields(
-          this.normalizeText(element.medico_nombre),
-          this.normalizeText(element.medico_apellido),
-          this.normalizeText(element.paciente_nombre),
-          this.normalizeText(element.paciente_apellido),
-          this.convertirFecha(element.turno_fecha),
-          element.turno_hora
-        ) +
-        "\n";
-    });
-    return message;
-  }
-
-  /**
-   * Converts a date from YYYY-MM-DD format to DD-MM-YYYY format.
-   * @param {string} date - The date in YYYY-MM-DD format.
-   * @returns {string} - The date in DD-MM-YYYY format.
-   */
-  private convertirFecha(date: string) {
-    // Split the date into year, month, and day parts
-    const [year, month, day] = date.split("-");
-
-    // Format the date into the new DD-MM-YYYY format
-    return `${day}-${month}-${year}`;
-  }
-
-  // Helper method to replace placeholders in the message template with actual values
-  private addTextFields = (
-    medico_nombre: string,
-    medico_apellido: string,
-    paciente_nombre: string,
-    paciente_apellido: string,
-    turno_fecha: string,
-    turno_hora: string
-  ) => {
-    return enviroments.MSJ.replace("<medico_nombre>", medico_nombre)
-      .replace("<medico_apellido>", medico_apellido)
-      .replace("<paciente_nombre>", paciente_nombre)
-      .replace("<paciente_apellido>", paciente_apellido)
-      .replace("<turno_fecha>", turno_fecha)
-      .replace("<turno_hora>", turno_hora);
-  };
-
-  // Helper method to normalize text by replacing accented characters and special symbols
-  private normalizeText(text: string) {
-    const accentsMap = new Map([
-      ["á", "a"],
-      ["é", "e"],
-      ["í", "i"],
-      ["ó", "o"],
-      ["ú", "u"],
-      ["Á", "A"],
-      ["É", "E"],
-      ["Í", "I"],
-      ["Ó", "O"],
-      ["Ú", "U"],
-      ["ñ", "n"],
-      ["Ñ", "N"],
-      // Add more if needed
-    ]);
-
-    const specialCharsMap = new Map([
-      ["@", "arroba"],
-      ["#", "num"],
-      ["$", "$"],
-      ["%", "porcentaje"],
-      ["&", "Y"],
-      ["*", "asterisco"],
-      // Add more if needed
-    ]);
-
-    let normalizedText = text;
-
-    // Replace accented characters with their non-accented counterparts
-    accentsMap.forEach((value, key) => {
-      const regex = new RegExp(key, "g");
-      normalizedText = normalizedText.replace(regex, value);
-    });
-
-    // Replace special characters with their textual representation
-    specialCharsMap.forEach((value, key) => {
-      const regex = new RegExp(`\\${key}`, "g"); // Escape special characters
-      normalizedText = normalizedText.replace(regex, value);
-    });
-
-    return normalizedText;
   }
 }
